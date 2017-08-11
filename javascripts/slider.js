b5057e86f336b26b3755d467ef8d19cad77586c8
Jayson Thompson <jaysonthompson922@gmail.com>
2017-08-11 09:32:03 -0700

    refactored code to make it more readable

--- javascripts/slider.js
+++ javascripts/slider.js
@@ -1,41 +1,36 @@
 'use strict';
 
-$(function(){
+$(function (){
 
-var width = 720;
-var animationSpeed = 1000;
-var intervalSpeed = 3000;
-var currentSlide = 1;
+	var width = 720;
+	var animationSpeed = 1000;
+	var intervalSpeed = 3000;
+	var currentSlide = 1;
+	var interval;
+	//DOM cache
+	var $slider = $('#slider');
+	var $slideContainer = $slider.find('.slides');
+	var $slide = $slideContainer.find('.slide');
 
-//DOM cache
-var $slider = $('#slider');
-var $slideContainer = $slider.find('.slides');
-var $slides = $slideContainer.find('.slide')
+	function startSlider(){
+		interval = setInterval(function(){
+			$slideContainer.animate({'margin-left': '-=' + width}, animationSpeed, function(){
+				currentSlide++;
+				if(currentSlide === $slide.length){
+					currentSlide = 1;
+					$slideContainer.css('margin-left', 0);
+				}
+			});
+		}, intervalSpeed);
+	}
 
-var interval;
+	function stopSlider(){
+		clearInterval(interval);
+	}
 
-function startSlider(){
-
-	interval =	setInterval(function(){
-		$slideContainer.animate(
-			{'margin-left': '-='+width}
-		, animationSpeed, function(){
-			if(++currentSlide === $slides.length){
-				currentSlide = 1;
-				$slideContainer.css('margin-left', 0);
-			}
-		});
-	}, intervalSpeed);
-}
-
-function pauseSlider(){
-	clearInterval(interval);
-}
-
-$slideContainer
-	.on('mouseenter', pauseSlider)
+	$slideContainer
+	.on('mouseenter', stopSlider)
 	.on('mouseleave', startSlider);
 
-startSlider();
-
+	startSlider();
 });
